097058401c53487d7322beca1fc2ca924f35d080
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-10-03 09:11:09 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// import components here
+
+// import modules here
+import saveDataModule from './modules/localstorage.js';
+import { loadStorageModule, removeStorageModule } from './modules/localstorage.js';
+
+// import utils here
+
+// write cool main js code here
+saveDataModule();
+    
+loadStorageModule();
+removeStorageModule();
